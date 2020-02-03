f328ea1f2057b8673698a2288ccc643640391259
Alex <masiuc1.19@gmail.com>
2020-02-03 17:52:05 +0300

    Ещу добавил дату

--- src/ApisDemo.java
+++ src/ApisDemo.java
@@ -2,5 +2,6 @@ public class ApisDemo {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println("Февраль 2020год.");
+        System.out.println("3 февраля");
     }
 }
